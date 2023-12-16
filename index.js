e7808eabca0ae3bc5bb0fe5b68acc780a3ea8d99
adnan <azizart320480@gamil.com>
2023-12-17 03:01:59 +0500

    api

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const mongoose = require ('mongoose');
 const dotenv = require ('dotenv');
 const helmet = require ('helmet');
 const morgan = require ('morgan');
+const cors = require('cors');
+
 const app = express();
 dotenv.config();
-const {MONGO_URL} = process.env;
+// const {MONGO_URL} = process.env;
+// const MONGO_URL = 'mongodb+srv://adnan:apple123@cluster0.afyprvm.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = 'mongodb://192.168.2.138:27017'
+// const MONGO_URL = 'mongodb://103.18.20.49:27017/test'
 
 mongoose.set("strictQuery", false);
 mongoose.connect(MONGO_URL, (err)=>{
@@ -20,6 +25,9 @@ const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postsRoute = require('./routes/posts');
 //middlewares
+
+app.use(cors())
+
 
 app.use(express.json());
 app.use(helmet());
