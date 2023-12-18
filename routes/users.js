f364b70e80d48071afdffa0e565f353433d3715b
adnan <azizart320480@gamil.com>
2023-12-18 19:00:18 +0500

    change delete api

--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.put('/:id', async (req, res)=>{
 
 //Delete user
 router.delete('/:id', async (req, res)=>{
-    if(req.body.userId === req.params.id || req.body.isAdmin){
+    // if(req.body.userId === req.params.id || req.body.isAdmin){
         try{
             const user = await User.findByIdAndDelete(req.params.id);
             res.status(200).json("Accont has been Deleted")
@@ -38,9 +38,9 @@ router.delete('/:id', async (req, res)=>{
             return res.status(500).json(err);
         }
 
-    } else{
-        return res.status(403).json("You can Delete your account only")
-    }
+    // } else{
+        // return res.status(403).json("You can Delete your account only")
+    // }
 });
 
 
