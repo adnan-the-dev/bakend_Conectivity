56ebb2433bb1bce6ec425ba16f8a668f5906f963
adnan <azizart320480@gamil.com>
2023-12-22 01:20:07 +0500

    some change

--- routes/users.js
+++ routes/users.js
@@ -3,35 +3,29 @@ const User = require('../models/User');
 const bcrypt = require("bcrypt");
 
 
-//update user
-// router.put('/:id', async (req, res)=>{
-//     if(req.body.userId === req.params.id || req.body.isAdmin){
-//         if(req.body.password){
-//             try{
-//                 const salt = await bcrypt.genSalt(10);
-//                 req.body.password = await bcrypt.hash(req.body.password, salt);
-//             } catch(err){ 
-//                 return res.status(500).json(err);
-//             }
-//         }
-//         try{
-//             const user = await User.findByIdAndUpdate(req.params.id, {
-//                 $set: req.body,
-//             });
-//             res.status(200).json("Accont has been Updated")
-//         } catch(err){
-//             return res.status(500).json(err);
-//         }
+// update user
+router.put('/:id', async (req, res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch(err){ 
+                return res.status(500).json(err);
+            }
+        }
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("Accont has been Updated")
+        } catch(err){
+            return res.status(500).json(err);
+        }
 
-//     } else{
-//         return res.status(403).json("You can Update your account only")
-//     }
-// });
-
-
-router.put('/update-user', async (req, res)=>{
-    let user = await User.findByIdAndUpdate(req.body._id, req.body)
-    res.json(user);
+    } else{
+        return res.status(403).json("You can Update your account only")
+    }
 });
 
 
