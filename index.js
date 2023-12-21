fe41c40703516d119cf7765f4f1d446e809b3545
adnan <azizart320480@gamil.com>
2023-12-22 00:38:46 +0500

    new btn catagory

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const cors = require('cors');
 
 const app = express();
 dotenv.config();
-// const {MONGO_URL} = process.env;
-// const MONGO_URL = 'mongodb+srv://adnan:apple123@cluster0.afyprvm.mongodb.net/?retryWrites=true&w=majority'
-const MONGO_URL = 'mongodb://192.168.2.138:27017'
-// const MONGO_URL = 'mongodb://103.18.20.49:27017/test'
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(MONGO_URL, (err)=>{
--- routes/users.js
+++ routes/users.js
@@ -4,29 +4,36 @@ const bcrypt = require("bcrypt");
 
 
 //update user
-router.put('/:id', async (req, res)=>{
-    if(req.body.userId === req.params.id || req.body.isAdmin){
-        if(req.body.password){
-            try{
-                const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(req.body.password, salt);
-            } catch(err){ 
-                return res.status(500).json(err);
-            }
-        }
-        try{
-            const user = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body,
-            });
-            res.status(200).json("Accont has been Updated")
-        } catch(err){
-            return res.status(500).json(err);
-        }
+// router.put('/:id', async (req, res)=>{
+//     if(req.body.userId === req.params.id || req.body.isAdmin){
+//         if(req.body.password){
+//             try{
+//                 const salt = await bcrypt.genSalt(10);
+//                 req.body.password = await bcrypt.hash(req.body.password, salt);
+//             } catch(err){ 
+//                 return res.status(500).json(err);
+//             }
+//         }
+//         try{
+//             const user = await User.findByIdAndUpdate(req.params.id, {
+//                 $set: req.body,
+//             });
+//             res.status(200).json("Accont has been Updated")
+//         } catch(err){
+//             return res.status(500).json(err);
+//         }
 
-    } else{
-        return res.status(403).json("You can Update your account only")
-    }
+//     } else{
+//         return res.status(403).json("You can Update your account only")
+//     }
+// });
+
+
+router.put('/update-user', async (req, res)=>{
+    let user = await User.findByIdAndUpdate(req.body._id, req.body)
+    res.json(user);
 });
+
 
 //Delete user
 router.delete('/:id', async (req, res)=>{
